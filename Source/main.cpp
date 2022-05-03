f23c5d5ca759ebe5ad11b876c9139cffc23d176e
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2022-05-03 14:14:00 +0200

    ifstream V1.1

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	List<MovieNode> L;
 	MovieNode* N;
-	string name;
-	int number = 1;
+	string name, sum;
+	int number;
 	float rating = 0.1;
 	ifstream file;
 	file.open("projekt2_dane.csv");
@@ -18,22 +18,58 @@ int main()
 
 	if (file.is_open())
 	{
-		file.ignore(1);
-		for (int i = 0; i < 1; i++)
-			//while (file << "m")
+		file.ignore(1000, '\n');
+		//file.ignore(100, '\n');
 
+
+		for (int i = 0; i < 10; i++)
+			//while (file << "m")
 		{
-			//	file.unget();
-			//getline(file, number, ',');
-			file >> number;
-			getline(file, name, ',');
-			file >> rating;
-			//getline(file, rating, ',');
-			N = new MovieNode(name, number, rating);
-			L.AddAtEnd(N);
-			//info += s;
+			if (file.good())
+			{
+				//	file.unget();
+				//getline(file, number, ',');
+				file >> number;
+				file.ignore(10, ',');
+
+				/*else
+				{
+					cout << "Dupa" << endl;
+
+					file.clear();
+					file.ignore(10, -1);
+				}*/
+
+				getline(file, name, ',');
+				//else
+				//{
+				//	cout << "zle" << endl;
+				//	//file.unget();
+				//	file.clear();
+				//	file.ignore(EOF);
+				//}
+
+				file >> rating;
+				file.ignore(10, '\n');
+				//else
+				//{
+				//	cout << "nie" << endl;
+				//	//file.unget();
+				//	file.clear();
+				//	file.ignore(EOF);
+				//}
+
+				sum += name;
+				//cout << number << name << rating << endl;
+				//cout << name << endl;
+				//cout << rating << endl;
+				//number = file.get();
+				//file.ignore(1, '\n');
+				//file >> rating;
+				//getline(file, rating, ',');
+				L.AddAtEnd(new MovieNode(name, number, rating));
+			}
 		}
-		cout << name << endl;
 		L.PrintList();
 	}
 
