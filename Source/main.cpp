7d658c23fe75c282b121f44ccd650be67c1408e2
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2022-05-04 13:36:29 +0200

    V2.0 clean

--- Source/main.cpp
+++ Source/main.cpp
@@ -17,50 +17,10 @@ int main()
 	//file.open("1.txt");
 	string help;
 
-	//c = file.get();
-	//cout << isdigit(file.peek()) << endl;
-	//for (int i = 0; i < 2; i++)
-	//{
-	//	while (!(file >> number))
-	//	{
-	//		file.clear();
-	//		file.ignore();
-	//	}
-	//	file.ignore(1000, ',');
-	//	while (!(getline(file, name, ',')))
-	//	{
-	//		file.clear();
-	//		file.ignore();
-	//	}
-	//	//file.ignore(1000, ',');
-
-	//	
-	//	//rating = file.get();
-	//	file >> rating;
-	//	cout << file.good() << endl;
-
-	//	/*while (!(file >> rating))
-	//	{
-	//		file.clear();
-	//		file.ignore();
-	//	}*/
-	//	file.ignore(1000, '\n');
-
-	//	//file.ignore(1000, '\n');
-	//	cout <<number << name << " " << rating << endl;
-	//}
-
-	//file.clear();
-	//cout << quoted(c, ',') << endl;
-
 	if (file.is_open())
 	{
-		//file.ignore(1000, '\n');
-		for (int i = 0; i < 50000; i++)
-		//while (file.peek() != file.eof())
+	for (int i = 0; i < 50000; i++)
 		{
-			//if (file.good())
-			//{
 
 			while (!(file >> number))
 			{
@@ -80,9 +40,6 @@ int main()
 				getline(file, help, ',');
 				name += help;
 			}
-			//cout << isdigit(file.peek()) << endl;
-			//cout << (!isdigit(file.peek())) << endl;
-			//file.ignore(1000, ',');
 
 			while (!(file >> rating))
 			{
@@ -90,22 +47,7 @@ int main()
 				file.ignore();
 			}
 			file.ignore(1000, '\n');
-
-			//cout << file.good() << endl;
-
-			//do
-			//{
-			//	if (!file.good())
-			//	{
-			//		file.ignore(1000, ',');
-			//		file.clear();
-			//	}
-			//	//if (!file.good()) file.ignore(1000, ',');
-			//} while (!file.good());
-			//cout << number << name << " " << rating << endl;
 			L.AddAtEnd(new MovieNode(name, number, rating));
-			//}
-			//else cout << "Problem z plikiem" << endl;
 		}
 		//cout << (file.peek() == EOF) << endl;
 		//cout << (file.peek() == file.eof()) << endl << L.Size() << endl;
@@ -113,7 +55,6 @@ int main()
 
 	}
 	else cout << "Plik nie jest otwarty.";
-
 
 	file.close();
 	return 0;
